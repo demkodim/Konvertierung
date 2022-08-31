c7c575d4fc96871bd4b5a66a05003440b4c61c00
demko <demko@DESKTOP-SUT4EJA>
2022-08-31 17:13:32 +0200

    Добавьте файлы проекта.

---
+++ Konvertierung/Program.cs
@@ -0,0 +1,25 @@
+﻿//Aufgabe 2
+string name = "Dimitri";
+Console.WriteLine($"Vorname: {name}");
+
+short age = 17;
+string alter = age.ToString();
+Console.WriteLine($"Alter: {alter}");
+
+string yes = "Ja";
+Console.WriteLine($"Ist C# die erste Programmiersprache, die Sie erlernen?: {yes}");
+
+
+//Aufgabe 3 
+int Dollarzahl = 36;
+char Dollar = (char)Dollarzahl;
+
+
+//Aufgabe 4
+string eingabe = Console.ReadLine();
+short short_Number = Convert.ToInt16(eingabe);
+Console.WriteLine(short_Number / 10);
+
+string eingabe2 = Console.ReadLine();
+double double_Number = Convert.ToDouble(eingabe2);
+Console.WriteLine(double_Number / 10);
